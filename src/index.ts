470e607cf094aa35e29069ebdf69286a012cea83
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-27 01:58:10 +0530

    updated

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+//
+// ThemesX
+// by ImKKingshuk
+// Git-  https://github.com/ImKKingshuk/ThemesX.git
+// Copyright © 2023 , @ImKKingshuk | All Rights Reserved.
+// GNU General Public License v3.0 or later
+//
+
+import ThemeProvider from "./ThemeProvider";
+import useTheme from "./useTheme";
+
+export { ThemeProvider, useTheme };
