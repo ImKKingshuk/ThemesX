59a88a28834c3d374c1af0c81b2d2c6155333980
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-27 01:59:00 +0530

    updated

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -1,3 +1,10 @@
+//
+// ThemesX
+// by ImKKingshuk
+// Git-  https://github.com/ImKKingshuk/ThemesX.git
+// Copyright © 2023 , @ImKKingshuk | All Rights Reserved.
+// GNU General Public License v3.0 or later
+//
 import React, { Fragment, useMemo, useContext, FC, ReactNode } from "react";
 import Theme from "./Theme";
 import ThemeContext from "./ThemeContext";
