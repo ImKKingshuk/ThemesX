0d344bd03f9b7664147920e6c909bcba6841ebcd
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-27 01:59:15 +0530

    updated

--- src/useTheme.tsx
+++ src/useTheme.tsx
@@ -1,3 +1,10 @@
+//
+// ThemesX
+// by ImKKingshuk
+// Git-  https://github.com/ImKKingshuk/ThemesX.git
+// Copyright © 2023 , @ImKKingshuk | All Rights Reserved.
+// GNU General Public License v3.0 or later
+//
 import { useContext } from "react";
 import ThemeContext, { UseThemeProps } from "./ThemeContext";
 
