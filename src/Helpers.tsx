94f435bb3cdbdd69995227e520b5597bcd8f51af
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-27 01:59:32 +0530

    updated

--- src/Helpers.tsx
+++ src/Helpers.tsx
@@ -1,3 +1,10 @@
+//
+// ThemesX
+// by ImKKingshuk
+// Git-  https://github.com/ImKKingshuk/ThemesX.git
+// Copyright © 2023 , @ImKKingshuk | All Rights Reserved.
+// GNU General Public License v3.0 or later
+//
 import React from "react";
 
 const getTheme = (key: string, fallback?: string) => {
