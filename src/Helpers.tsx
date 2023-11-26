8dcbf6db3937ebcb0f5a9a1efdc853fd435ce52d
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-27 00:34:12 +0530

    updated

---
+++ src/Helpers.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const getTheme = (key: string, fallback?: string) => {
+  if (isServer) return undefined;
+  let theme;
+  try {
+    theme = localStorage.getItem(key) || undefined;
+  } catch (e) {
+    // Unsupported
+  }
+  return theme || fallback;
+};
+
+const disableAnimation = () => {
+  const css = document.createElement("style");
+  css.appendChild(
+    document.createTextNode(
+      `*{-webkit-transition:none!important;-moz-transition:none!important;-o-transition:none!important;-ms-transition:none!important;transition:none!important}`
+    )
+  );
+  document.head.appendChild(css);
+
+  return () => {
+    (() => window.getComputedStyle(document.body))();
+
+    setTimeout(() => {
+      document.head.removeChild(css);
+    }, 1);
+  };
+};
+
+const getSystemTheme = (e?: MediaQueryList | MediaQueryListEvent) => {
+  if (!e) e = window.matchMedia(MEDIA);
+  const isDark = e.matches;
+  const systemTheme = isDark ? "dark" : "light";
+  return systemTheme;
+};
+
+export { getTheme, disableAnimation, getSystemTheme };
