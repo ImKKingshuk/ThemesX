3ee665a8dece43fcfe777366a47c6d240a9a2e6c
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-27 01:58:51 +0530

    updated

--- src/ThemeContext.tsx
+++ src/ThemeContext.tsx
@@ -1,3 +1,10 @@
+//
+// ThemesX
+// by ImKKingshuk
+// Git-  https://github.com/ImKKingshuk/ThemesX.git
+// Copyright © 2023 , @ImKKingshuk | All Rights Reserved.
+// GNU General Public License v3.0 or later
+//
 import { createContext, Dispatch, SetStateAction } from "react";
 
 export interface UseThemeProps {
