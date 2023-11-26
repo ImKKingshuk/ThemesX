46d57b78165e5ab1cd1e9d692632f8f90072bcd5
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-27 01:43:45 +0530

    updated

--- ThemesXBeta/useTheme.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import ThemeContext, { UseThemeProps } from "./ThemeContext";
-
-const useTheme = (): UseThemeProps =>
-  useContext(ThemeContext) || { setTheme: (_) => {}, themes: [] };
-
-export default useTheme;
